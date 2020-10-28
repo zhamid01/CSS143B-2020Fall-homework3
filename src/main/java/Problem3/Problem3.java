df605e2fb4ada8e0881adaef944cd4474e76ac37
zhamid01 <zhamid01@uw.edu>
2020-10-27 21:35:11 -0700

    Added listnode and Problem3Test edit

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -8,21 +8,19 @@ import Problem2.ListNode;
 import java.io.PrintStream;
 import java.util.List;
 
-/*
-I was not able to get a successful test. What I wanted to do here was go through and add put each node into
-my reverse LinkedList stack using a for loop. Then I wanted to go through the reverse LinkedList Stack and
-print the return value of the reverse.pop.
- */
 public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
         if (list == null) {
             return;
-        } else {
-            LinkedListStack reverse = new LinkedListStack();
-            for (ListNode i = list; i != null; i = i.next) {
-                reverse.push(i);
+        }
+        else {
+            Stack<Integer> reverse = new LinkedListStack<>();
+            ListNode n1 = list.next;
+            while (n1 != null) {
+                reverse.push(n1.val);
+                n1 = n1.next;
             }
-            for (int i = reverse.size(); i > 0; i--) {
+            while (reverse.size() != 0) {
                 print.println(reverse.pop());
             }
         }
